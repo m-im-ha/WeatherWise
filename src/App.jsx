7b6b0249ac3816a2d0c62b5fd9dc1817cba54243
imran <sherlockholmesad221@gmail.com>
2024-10-20 15:41:45 +0600

    edit App.jsx to get weather data

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,28 @@ function App() {
 
   useEffect(() => {
     async function fetchWeather() {
-      const geoResponse = await axios.get(
-        `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
-      );
-      const { country, country_code, id, latitude:lat, longitude:lng, name:cityName } =
-        geoResponse.data.results[0];
-        console.log(country, country_code, id, lat, lng, cityName)
+      if (!location) return;
+      try {
+        const geoResponse = await axios.get(
+          `https://geocoding-api.open-meteo.com/v1/search?name=${location}`
+        );
+        const {
+          country,
+          country_code,
+          id,
+          latitude: lat,
+          longitude: lng,
+          name: cityName,
+          timezone,
+        } = geoResponse.data.results[0];
+        console.log(country, country_code, id, lat, lng, cityName);
+        const weatherResponse = await axios.get(
+          `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lng}&current=temperature_2m,apparent_temperature,is_day,precipitation,rain,showers,wind_speed_10m,wind_direction_10m&daily=temperature_2m_max,temperature_2m_min&timezone=${timezone}`
+        );
+        console.log(weatherResponse);
+      } catch (error) {
+        console.error(error);
+      }
     }
     fetchWeather();
   }, [location]);
