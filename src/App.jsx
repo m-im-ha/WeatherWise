f7540a281bafaa354064cf80cd655628cbfd5fb1
imran <sherlockholmesad221@gmail.com>
2024-10-20 09:28:02 +0600

    edit package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-4xl text-pink-950 text-center">Hello Weather</h1>
+      <h1 className="text-4xl text-pink-950 text-center font-extrabold">Hello Weather</h1>
     </div>
   );
 }
